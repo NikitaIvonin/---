74b1cb73b0619f1a5a347af8f30041dbcfb4b639
Nikita <nivonin4@gmail.com>
2024-02-21 09:17:06 +0300

    Added superuser

--- auto_store_project/auto_store/views.py
+++ auto_store_project/auto_store/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
+def build_template(lst: list, cols: int) -> list[list]:
+    new_list = []
+    for i in range(0, len(lst), cols):
+        new_list.append(lst[i:i + cols])
+    return new_list
+
+build_template([1,2,3,4,5,6,7,8,9,10], 3)
+
 def product_list(request):
-    return HttpResponse("<h1>Hello</h1>")
+    products = Product.objects.all()
+    return render(request, 'auto_store/product_list.html', context={'product_list': build_template(products, 3)})
